311ca08182a54a60c75d9b95465dbde64c017660
Jordan Newberry <jordan.newberry@gmail.com>
2021-03-09 11:23:47 -0600

    setting up the pool for the back end

---
+++ server/server.js
@@ -0,0 +1,38 @@
+// express.js import
+const express = require('express');
+
+// dotenv config
+require('dotenv').config();
+
+const app = express();
+const bodyParser = require('body-parser');
+
+// auth stuff
+const sessionMiddleware = require();
+const passport = require();
+
+// route includes
+
+// set up body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Passport session configuration
+app.use(sessionMiddleware);
+
+// start Passport session
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+
+// Serve static files
+app.use(express.static('build'));
+
+// set listening Port
+const PORT = process.env.PORT || 5000;
+
+// Listen!
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}...`)
+})

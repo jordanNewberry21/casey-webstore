bc69ffde5142f21633b33b14df240cf1b3fe9e02
Jordan Newberry <jordan.newberry@gmail.com>
2021-03-09 14:38:36 -0600

    set up sagas and reducers for login and register functions

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
+import userReducer from './userReducer';
+import errors from './errors.reducer';
 
 // root.reducer is where all reducers will live
 const rootReducer = combineReducers ({
-    
+    userReducer,
+    errors,
 });
 
 export default rootReducer;
--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -3,6 +3,6 @@ import { all } from 'redux-saga/effects';
 // root.saga is where all the sagas will live to keep it all in one place
 export default function* rootSaga() {
     yield all([
-
+        
     ]);
 };

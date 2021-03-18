08d4f4f0b394bbef3efcbb22b34cefe073894335
Jordan Newberry <jordan.newberry@gmail.com>
2021-03-18 10:13:25 -0500

    added ProtectedRoutes and AdminRoutes on the front-end for redirecting traffic for logged in users and admin

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,6 +11,9 @@ import './App.css';
 
 // components
 import Nav from '../Nav/Nav';
+import AdminRoute from '../AdminRoute/AdminRoute';
+import AdminHome from '../AdminHome/AdminHome';
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
 import Inventory from '../Inventory/Inventory';
@@ -28,20 +31,41 @@ class App extends Component {
                 <Switch>
                 {/* Visiting localhost:3000 will redirect to localhost:3000/store */}
                 <Redirect exact from="/" to="/store" />
+            
                 <Route
-                    exact
-                    path="/register"
-                    component={RegisterPage}
-                 />
-                 <Route
-                    exact
-                    path="/login"
-                    component={LoginPage}
-                 />
-                 <Route
                     exact
                     path="/store"
                     component={Inventory}
+                />
+                <ProtectedRoute
+                    // with authRedirect:
+                    // - if logged in, redirects to "/user"
+                    // - if admin, redirect to "/admin"
+                    // - else shows LoginPage at /login
+                    exact
+                    path="/login"
+                    component={LoginPage}
+                    authRedirect="/user"
+                    adminRedirect="/admin"
+                />
+                <ProtectedRoute
+                    // with authRedirect:
+                    // - if logged in, redirects to "/user"
+                    // - if admin, redirect to "/admin"
+                    // - else shows RegisterPage at "/registration"
+                    exact
+                    path="/registration"
+                    component={RegisterPage}
+                    authRedirect="/user"
+                    adminRedirect="/admin"
+                />
+                <AdminRoute
+                    // AdminRoute ensures the user is an admin
+                    // if not logged in, redirects to login
+                    // if logged in but not admin, redirects to user homepage
+                    exact
+                    path="/admin"
+                    component={AdminHome}
                 />
                 </Switch>
             </div>

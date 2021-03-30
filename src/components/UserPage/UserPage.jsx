de88141e967665b371c244845edafbf481ab98ba
Jordan Newberry <jordan.newberry@gmail.com>
2021-03-30 09:21:20 -0500

    moving stuff around

---
+++ src/components/UserPage/UserPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import AdminHome from '../AdminHome/AdminHome';
+import UserPageStandard from './UserPageStandard.jsx';
+
+
+
+function UserPage (props) {
+    
+  // grabbing the application info from the redux store
+  const user = useSelector((store) => store.user);
+
+  return (
+    <div>
+      {
+        user.admin ?
+        <AdminHome /> :
+        <UserPageStandard />
+      }
+    </div>
+  );
+  
+}
+
+export default UserPage;

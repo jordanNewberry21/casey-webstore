f83ce714bacc65aa6719221ff535b5aa18966dfe
Jordan Newberry <jordan.newberry@gmail.com>
2021-04-27 13:44:34 -0500

    added checkout button to nav menu

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -3,14 +3,19 @@ import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import './Nav.css';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import Total from './total';
 
 export default function Nav() {
-    const user = useSelector(state => state.user)
+    const cart = useSelector(state => state.cart);
+    const user = useSelector(state => state.user);
     return (
         <div className="nav">
             <h2 className="nav-title">Creations by Casey</h2>
             <div className="nav-right">
-            <Link className="nav-link" to="/about">
+                <Link className="nav-link" to="/checkout">
+                    Checkout ({cart.length}) <Total cart={cart} />
+                </Link>
+                <Link className="nav-link" to="/about">
                     About
                 </Link>
                 <Link className="nav-link" to="/register">

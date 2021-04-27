42ab4a2e60904566b9581672878df0cf6218052c
Jordan Newberry <jordan.newberry@gmail.com>
2021-04-26 22:20:21 -0500

    fixed css bug that caused headers on login page to left align.

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -2,17 +2,19 @@ import React from 'react';
 import { useHistory } from 'react-router-dom';
 import LoginForm from './LoginForm/LoginForm';
 import { Card, Container, Button, Row } from 'react-bootstrap';
+import './LoginPage.css';
 
 function LoginPage() {
     const history = useHistory();
 
     return (
         <Container>
-            <Row>
+            <Row style={{ flexDirection: "column" }}>
                 <div>
                     <Card.Header className="card-header">
                         <h3>Welcome to my store</h3>
                     </Card.Header>
+                    
                     <Card.Body className="card-body">
                         <h2>Creations by Casey</h2>
                     </Card.Body>
